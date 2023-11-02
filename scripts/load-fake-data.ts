4394a9b8dcf209695d451876c86adea82a64dea2
Daniel Akinniranye <dakinnir@gmail.com>
2023-11-01 22:51:49 -0400

    db connection

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,5 +1,24 @@
-function loadFakeData(newUsers: number = 5) {
+import { Client } from 'pg';
+import { loadEnvConfig } from '@next/env';
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+async function loadFakeData(newUsers: number = 5) {
   console.log(`executing load fake data, generating ${newUsers} new users`);
+
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
+
+  await client.connect();
+  const res = await client.query('select 1');
+  console.log(res);
+  await client.end();
 }
 
 loadFakeData(10);
